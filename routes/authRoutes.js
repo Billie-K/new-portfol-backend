1608bf765e6fe26351293caf9a0748ea9dccf517
Billie-K <billiemush@gmail.com>
2023-12-11 18:35:57 +0200

    no rate limit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,20 @@
 import express from "express";
-import { rateLimit } from "express-rate-limit";
+// import { rateLimit } from "express-rate-limit";
 import { register, signIn, test, SendVerificationEmail } from "../controllers/authController.js";
 
-//ip rate limit
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
+// //ip rate limit
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// });
 
 const router = express.Router();
 
 // Register routes
-router.post("/register", limiter, register);
+router.post("/register", register);
+// router.post("/register", limiter, register);
 router.post("/login", signIn);
 router.post("/test", test);
 router.post('/:id/verify/:token/', SendVerificationEmail
